062080f168e952ade5443103d41f0de93005a0a8
AhmedMohsenTaha <ahmed.taha@flairstech.com>
2025-07-16 11:15:50 +0300

    Add BaseEntity.

---
+++ src/Services/Catalog/Catalog.API/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedOn{ get; set; }
+    public string CreatedBy { get; set; } = default!;
+    public DateTime UpdaetedOn { get; set; }
+    public string UpdatedBy { get; set; } = default!;
+    public bool IsDeleted { get; set; }
+}
