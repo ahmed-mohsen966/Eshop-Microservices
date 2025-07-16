37c5fe2c15121e2ed387a653baab2cbd2364b163
AhmedMohsenTaha <ahmed.taha@flairstech.com>
2025-07-16 12:48:14 +0300

    add private init factory method for product entity.

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -7,4 +7,25 @@ public class Product : BaseEntity
     public List<string> Category { get; set; } = new();
     public string ImageFile { get; set; } = default!;
     public decimal Price { get; set; }
+
+    public Product()
+    {
+        
+    }
+
+    private Product(string name, string description, List<string> category, string imageFile, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Category = category;
+        ImageFile = imageFile;
+        Price = price;
+    }
+
+    #region Factory Methods
+
+    public static Product init(string name, string description, List<string> category, string imageFile, decimal price)
+        => new Product(name,description,category,imageFile,price);
+
+    #endregion
 }
