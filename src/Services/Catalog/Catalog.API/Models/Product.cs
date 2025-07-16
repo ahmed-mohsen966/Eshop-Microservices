7f731d781345ee8880618864024f8eee2fea891f
AhmedMohsenTaha <ahmed.taha@flairstech.com>
2025-07-16 11:19:34 +0300

    Add product entity.

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Models;
+
+public class Product : BaseEntity
+{
+    public string Name { get; private set; } = default!;
+    public string Description { get; private set; } = default!;
+    public List<string> Category { get; set; } = new();
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
