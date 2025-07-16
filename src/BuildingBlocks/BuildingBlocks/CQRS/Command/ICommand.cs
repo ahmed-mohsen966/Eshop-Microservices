e9520485c88cb645a6182cd757a58a8046c7071e
AhmedMohsenTaha <ahmed.taha@flairstech.com>
2025-07-16 12:25:23 +0300

    Add Abstraction layers for CQRS building block.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Command/ICommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS.Command;
+
+public interface ICommand : ICommand<Unit> { }
+public interface ICommand<out TResponse> : IRequest<TResponse> { }
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Query/IQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS.Query;
+
+public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull { }
