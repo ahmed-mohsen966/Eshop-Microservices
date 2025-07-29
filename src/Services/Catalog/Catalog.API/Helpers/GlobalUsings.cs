6bf5663e9ea8da1fca94ec5e9eb6de540e16fc13
AhmedMohsenTaha <ahmed.taha@flairstech.com>
2025-07-29 13:48:03 +0300

    add marten library to convert postgresql to documnetDb

--- src/Services/Catalog/Catalog.API/Helpers/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/Helpers/GlobalUsings.cs
@@ -1,5 +1,9 @@
 ﻿global using Mapster;
 global using Carter;
 global using MediatR;
+global using Marten;
 
 global using Catalog.API.Products.Commands.CreateProduct;
+global using BuildingBlocks.CQRS.Command;
+global using Catalog.API.Models;
+
--- src/Services/Catalog/Catalog.API/Products/Commands/CreateProduct/Handler.cs
+++ src/Services/Catalog/Catalog.API/Products/Commands/CreateProduct/Handler.cs
@@ -1,18 +1,19 @@
-﻿using BuildingBlocks.CQRS.Command;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.Commands.CreateProduct;
+﻿namespace Catalog.API.Products.Commands.CreateProduct;
 
 public record CreateProductCommand(string Name,List<string> Category,string Description
     ,string Imagefile, decimal Price) :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class Handler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class Handler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        var product = Product.init(command.Name,command.Description,command.Category,command.Imagefile,command.Price);
+        var product = Product.init(command.Name,command.Description
+                                ,command.Category,command.Imagefile,command.Price);
 
-        return new CreateProductResult(Guid.NewGuid());
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
     }
 }
